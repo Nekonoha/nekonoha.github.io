c0297593c7b4514e302ae1f230e186834fe05dd1
Nekonoha <tan.fantazma@gmail.com>
2017-12-25 14:32:28 +0900

    turai

--- web/js/loading.js
+++ web/js/loading.js
@@ -1,7 +1,5 @@
 $(function() {
     var h = $(window).height();
-
-    $('#wrap').css('display', 'none');
     $('#loader-bg ,#loader').height(h).css('display', 'block');
 });
 
@@ -17,5 +15,4 @@ $(function() {
 function stopload() {
     $('#wrap').css('display', 'block');
     $('#loader-bg').delay(900).fadeOut(800);
-    $('#loader').delay(600).fadeOut(300);
 }
